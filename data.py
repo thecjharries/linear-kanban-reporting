6c6072c0b7c7ae2a1d3b6b3f5b7c992955ccbc59
CJ Harries <cj@wizardsoftheweb.pro>
2023-03-26 10:07:05 -0500

    Document LinearClient

--- data.py
+++ data.py
@@ -12,7 +12,14 @@ URL: str = 'https://api.linear.app/graphql'
 
 
 class LinearClient:
+    """A convenience wrapper around gql for Linear's GraphQL API."""
+
     def __init__(self, token: str):
+        """
+        Initialize the client.
+
+        :param token: The API key for the Linear workspace.
+        """
         self.token: str = token
         self.transport: RequestsHTTPTransport = RequestsHTTPTransport(
             url=URL,
@@ -26,9 +33,24 @@ class LinearClient:
                                      fetch_schema_from_transport=True)
 
     def execute(self, query: DocumentNode, variable_values: Dict[str, Any] = None) -> Dict[str, Any]:
+        """
+        Execute a GraphQL query.
+
+        :param query: The query to execute.
+        :param variable_values: The variables to use in the query.
+        :return: The result of the query.
+        """
         return self.client.execute(query, variable_values=variable_values)
 
     def paginate(self, query: DocumentNode, path: Tuple[str], variable_values: Dict[str, Any] = None) -> Dict[str, Any]:
+        """
+        Paginate through a GraphQL query.
+
+        :param query: The query to execute.
+        :param path: The path to the page info in the result.
+        :param variable_values: The variables to use in the query.
+        :return: The result of the query.
+        """
         page_info = {
             'hasNextPage': True,
             'endCursor': None
@@ -40,6 +62,15 @@ class LinearClient:
             yield result
 
     def drain(self, query: DocumentNode, desired_path: Tuple[str], page_info_path: Tuple[str], variable_values: Dict[str, Any] = None) -> List[Any]:
+        """
+        Drain a GraphQL query.
+
+        :param query: The query to execute.
+        :param desired_path: The path to the desired data in the result.
+        :param page_info_path: The path to the page info in the result.
+        :param variable_values: The variables to use in the query.
+        :return: The desired data from the query.
+        """
         result = list()
         for page in self.paginate(query, page_info_path, variable_values=variable_values):
             result.extend(reduce(dict.get, desired_path, page))
